975449f84632b970eb718501b6e64959ab2021ac
José Lucas <jltorquato12@gmail.com>
2021-01-05 11:17:10 -0300

    fixing bugs 2

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['app-gestao-cliente.herokuapp.com']
+ALLOWED_HOSTS = ['cliente-gestao-teste.herokuapp.com']
 
 
 # Application definition
